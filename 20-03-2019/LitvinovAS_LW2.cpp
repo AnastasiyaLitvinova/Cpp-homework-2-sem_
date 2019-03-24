1190dbb0c8e2c185dceb5105bc86295a3895379f
AnastasiyaLitvinova <44208865+AnastasiyaLitvinova@users.noreply.github.com>
2019-03-24 15:35:39 +0300

    Add files via upload

---
+++ 20-03-2019/LitvinovAS_LW2.cpp
@@ -0,0 +1,150 @@
+/*
+По максимальному элементу
+Если четное число строк - бабл сорт, нечетное - квик сорт
+*/
+
+
+#include <iostream>
+#include<algorithm>
+#include<vector>
+
+void swap(int*& a, int*& b);
+int getMax(int* arr, int size);
+void bubbleSort(int** arr, int n, int m);
+int* findLine(int** matrix, int n, int m, int key);
+int** getMatrix(int n, int m);
+void freeMemory(int** matrix, int n);
+void setElemenets(int** matrix, int n, int m);
+void display(int** matrix, int n, int m);
+int* getArr(int* arr, int size);
+void setMatrix(int**& matrix, int**& mtrx);
+void arrsort(int** matrix, int n, int m);
+
+int main()
+{
+	std::cout << "Enter the sizes of matrix: Ax*m" << std::endl;
+	int n, m;
+	std::cin >> n >> m;
+	int** matrix = getMatrix(n, m);
+	setElemenets(matrix, n, m);
+	display(matrix, n, m);
+	std::cout << std::endl;
+	arrsort(matrix, n, m);
+	std::cout << std::endl;
+	freeMemory(matrix, n);
+}
+
+void swap(int*& a, int*& b)
+{
+	int* t = a;
+	a = b;
+	b = t;
+}
+
+int getMax(int* arr, int size)
+{
+	int max = 0;
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (max < arr[i])
+			max = arr[i];
+	}
+	return max;
+}
+
+void bubbleSort(int** arr, int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - i - 1; ++j) {
+			if (getMax(arr[j], m) > getMax(arr[j + 1], m)) {
+				swap(arr[j], arr[j + 1]);
+			}
+
+		}
+	}
+}
+
+int* findLine(int** matrix, int n, int m, int key) {
+	for (int i = 0; i < n; ++i) {
+		if (getMax(matrix[i], m) == key)
+			return matrix[i];
+	}
+}
+
+int** getMatrix(int n, int m) {
+	int** matrix = new int*[n];
+	for (int i = 0; i < n; ++i) {
+		matrix[i] = new int[m];
+	}
+
+	return matrix;
+}
+
+void freeMemory(int** matrix, int n) {
+	for (int i = 0; i < n; ++i)
+		delete[] matrix[i];
+
+	delete[] matrix;
+}
+
+void setElemenets(int** matrix, int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cin >> matrix[i][j];
+		}
+	}
+}
+
+void display(int** matrix, int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int* getArr(int* arr, int size) {
+	int* a = new int[size];
+	for (int i = 0; i < size; ++i) {
+		a[i] = arr[i];
+	}
+
+	return a;
+}
+
+void setMatrix(int**& matrix, int**& mtrx) {
+	matrix = mtrx;
+}
+
+void arrsort(int** matrix, int n, int m) {
+	if (n % 2 == 0) {
+		bubbleSort(matrix, n, m);
+		display(matrix, n, m);
+	}
+	else {
+		std::vector<int> vect(n);
+		for (int i = 0; i < n; ++i) {
+			int term = getMax(matrix[i], m);
+			vect[i] = term;
+		}
+		std::sort(vect.begin(), vect.end());
+
+		int** mtrx = new int*[n];
+		for (int i = 0; i < n; ++i) {
+			mtrx[i] = new int[m];
+			for (int j = 0; j < n; ++j) {
+				if (getMax(matrix[j], m) == vect[i])
+					for (int q = 0; q < m; ++q) {
+						mtrx[i][q] = matrix[j][q];
+					}
+			}
+		}
+
+		display(mtrx, n, m);
+
+		freeMemory(mtrx, n);
+
+
+	}
+}
